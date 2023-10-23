fc50e82a04510dc79a15ae9be176f1bbef97dbac
Drifty-Cyber <raphaelfadimu@gmail.com>
2023-10-23 22:51:07 +0100

    using event listeners & handlers ��

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+  const handleClick = () => {
+    console.log('Clicked Button');
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log(`Clicked by ${name}`);
+    console.log(e);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={(e) => handleClickAgain('Ralph', e)}>Click Again</button>
     </div>
   );
 };
