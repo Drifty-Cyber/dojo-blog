cdc4db9c77e47377ce3e865bf99e268077f7235c
Drifty-Cyber <raphaelfadimu@gmail.com>
2023-10-23 20:04:02 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
-function App() {
+export function App() {
+  // const title = 'Welcome to my Blog';
+  // const likes = 50;
+  // const link = `https://www.google.com`;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
 
-export default App;
+export function MyButton() {
+  return <button>I'm a button</button>;
+}
+
+// export default App;
